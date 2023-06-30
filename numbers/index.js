ee6d60b81e7c0c6f77bb8de481825c4aa6295b98
nijuyonkadesu <24ksriramv@gmail.com>
2023-06-30 21:12:58 +0530

    parse response from urls and save them

--- numbers/index.js
+++ numbers/index.js
@@ -1,13 +1,34 @@
+const axios = require('axios');
+const http = require('http')
 const express = require('express')
 const app = express()
 const port = 3000
 
+gotSoFar = []
 
 app.get('/', (req, res) => {
     res.send("hello")
+    axios.get("http://104.211.219.98/numbers/primes")
+    .then((response) => {
+        console.log(response)
+    })
+})
+
+app.get('/numbers', async (req, res) => {
+    urls = req.query
+    await urls.url.forEach(url => {
+        axios.get(url)
+        .then(function (response) {
+            gotSoFar += response.data.numbers
+            console.log(gotSoFar)
+        })
+    })
+    
+    console.log(gotSoFar)
+    res.send(gotSoFar)
 })
 
 app.listen(port, () => {
-    console.log("Listening $port")
+    console.log(`Listening ${port}`)
 })
 
